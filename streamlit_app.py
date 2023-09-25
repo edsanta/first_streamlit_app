73d29c8a2ae9caf04b2183087555fb0d531c76c5
Eduardo Marcelino López Santamaría <62036039+edsanta@users.noreply.github.com>
2023-09-25 13:51:55 -0600

    Update streamlit_app.py
    typo frutis

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-st.dataframe(fruist_to_show)
+st.dataframe(fruits_to_show)
